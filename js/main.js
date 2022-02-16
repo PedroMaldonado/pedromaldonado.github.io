035cd83e8d13b7b9e0b1309bd4194ae12af55c87
Pedro Maldonado <pedrosms9@gmail.com>
2022-02-16 17:15:51 -0600

    buttons added

--- js/main.js
+++ js/main.js
@@ -63,9 +63,84 @@ $(document).ready(function () {
         }
     });
 
-     // animation scroll js
+    // animation scroll js
     var html_body = $('html, body');
     $('#blog h3 a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('#Frappes a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('#Smoothies a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('#Crepas a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('#price7 a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('#price8 a').on('click', function () { //use page-scroll class in any HTML tag for scrolling
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
